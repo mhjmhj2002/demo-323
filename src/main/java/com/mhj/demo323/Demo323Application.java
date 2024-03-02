472570a467829c87f606d7e42d277d25e9fa1c3b
MHJ <mhjmhj2002@gmail.com>
2024-03-02 18:01:46 -0300

    add source

---
+++ src/main/java/com/mhj/demo323/Demo323Application.java
@@ -0,0 +1,13 @@
+package com.mhj.demo323;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo323Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo323Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhj/demo323/Demo323ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhj.demo323;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo323ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
